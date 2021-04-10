9ccd8c2afd9c40d220748b8aed0364182da7f316
mrhrifat <mrhrifat383759@gmail.com>
2021-04-10 11:51:25 +0600

    Adding files

---
+++ 1 Days/05_LetConst.js
@@ -0,0 +1,9 @@
+function performAction(r) {
+    let area;
+    const pi = Math.PI;
+    let perimeter;
+    console.log(area = pi * r * r);
+    console.log(perimeter = 2 * pi * r);
+
+}
+performAction(2.6)
