c831eee0b4e25b7a683aa7c8aecfe56544d29ec4
mrhrifat <mrhrifat383759@gmail.com>
2021-04-10 15:35:31 +0600

    Adding files

---
+++ 2 Days/08_Loops.js
@@ -0,0 +1,14 @@
+function loopAction(str) {
+    var vowel = "aeiou";
+    var constant = "";
+    for (var i = 0; i <= str.length - 1; i++) {
+        if (vowel.includes(str[i])) {
+            console.log(str[i]);
+        } else {
+            constant = constant + str[i] + "\n";
+        }
+
+    }
+    console.log(constant)
+}
+loopAction("javascriptloop")
---
+++ 3 Days/09_Array.js
@@ -0,0 +1,8 @@
+//Unsolved
+function arrayAction(num) {
+    var max1 = Math.max(num)
+    console.log(max1)
+    return arguments;
+
+}
+arrayAction(4, 5, 6)
