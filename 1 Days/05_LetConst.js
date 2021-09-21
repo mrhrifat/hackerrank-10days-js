33374a427555cd67819180ab29b42ddf4b14e885
Rakibul Hassan Rifat <45077312+mrhrifat@users.noreply.github.com>
2021-09-22 01:46:46 +0600

    Delete

--- 1 Days/05_LetConst.js
+++
@@ -1,9 +0,0 @@
-function performAction(r) {
-    let area;
-    const pi = Math.PI;
-    let perimeter;
-    console.log(area = pi * r * r);
-    console.log(perimeter = 2 * pi * r);
-
-}
-performAction(2.6)
